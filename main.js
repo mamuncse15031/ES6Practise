398f9ec5b4a2ce036be643ef795a9764562def6e
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-06 02:26:54 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
 "use strict";
 
-var bangladesh = ["Dhaka", "Rajshahi", "Ctg"]
-var india = ["Mumbai", "Dilli", "Kalkata"]
-var china = ["Uhan", "Shanghai", "Hongkong"]
-
-var asia = [bangladesh, india, china];
-
-console.log(asia[2][1]);
