277575517895cc9641f1f7dc239e63ebc66c84d5
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-05 10:01:15 +0600

    learn object and nested object

---
+++ object.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var billgates = {shirt:true, shirtColor:'offWhite', smile:true, coshma: true, Sweater:true, SweaterColor:'gray'};
+
+console.log(billgates['shirtColor','coshma', 'SweaterColor']);
+
+var billgatesPro = {
+
+    shirt:{
+        color:'offWhite',
+        quality:'good',
+        price:'200usd'
+    },
+    Sweater:{
+        color:'gray',
+        quality:'good',
+        price:'230usd'
+    },
+    face:{
+        smiling:'yes',
+        choshma:'yes',
+        teeth:'white, big'
+    }
+}
+console.log(billgatesPro['face']);
